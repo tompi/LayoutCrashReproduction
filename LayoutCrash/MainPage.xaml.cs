7ee77c0eb290bdbc011c18548f3ab7e58822c0a7
Thomas Haukland <thomas.haukland@gmail.com>
2021-02-05 09:03:08 +0100

    Initial commit

---
+++ LayoutCrash/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace LayoutCrash
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                // Put a breakpoint on this line:
+                var size = Lably.FontSize;
+                // Try stepping to this line:
+                var thisWontHappen = size + 2;
+            });
+        }
+    }
+}
